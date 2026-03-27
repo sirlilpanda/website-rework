c86cc3cd714d3a7aca0cbb7d2e5d6c0b1b9bd653
sirlilpanda <me@sirlilpanda.studio>
2026-03-27 22:41:38 +1300

    new style

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,3 +8,12 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
 	interface Locals extends Runtime {}
 }
+
+interface Window {
+  theme: {
+    setTheme: (theme: "auto" | "dark" | "light") => void;
+    getTheme: () => "auto" | "dark" | "light";
+    getSystemTheme: () => "light" | "dark";
+    getDefaultTheme: () => "auto" | "dark" | "light";
+  };
+}
